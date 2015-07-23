4c9b8901dbe3066396bafad71978962380635f25
alwesam <alwesam@gmail.com>
2015-07-23 02:03:20 -0700

    fixed challenge model to validate for coordinates after saving

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -5,7 +5,11 @@ class Challenge < ActiveRecord::Base
 
   # geocoded_by / geocode methods come from the Geocoder gem
   geocoded_by :address
-  after_validation :geocode          # auto-fetch coordinates
+  before_validation :geocode          # auto-fetch coordinates
+
+  validates :address, presence: true
+  validates :latitude, presence: true
+  validates :longitude, presence: true
 
   #accepts_nested_attributes_for :questions, allow_destroy: true,
   #  reject_if: lambda {|x| x[:content].empty? }
